159ee0712e9f9ff438e6356d6a447588189b311a
bentmar <bjorn.bentmar@gmail.com>
2017-01-17 09:52:30 +0100

    first

---
+++ CardViewRenderer.cs
@@ -0,0 +1,51 @@
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using CoreGraphics;
+using movinspire.iOS.Renderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Frame), typeof(CardViewRenderer))]
+namespace movinspire.iOS.Renderers
+{
+   /* Elegant looking frame on iOS
+      with minor modifications to this post http://stackoverflow.com/a/40820035 */
+    public class CardViewRenderer : FrameRenderer
+    {
+        public override void Draw(CGRect rect)
+        {
+            SetupShadowLayer();
+            base.Draw(rect);
+        }
+
+        void SetupShadowLayer()
+        {
+            Layer.CornerRadius = 2; // 5 Default
+            if (Element.BackgroundColor == Xamarin.Forms.Color.Default)
+            {
+                Layer.BackgroundColor = UIColor.White.CGColor;
+            }
+            else
+            {
+                Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
+            }
+
+            Layer.ShadowRadius = 2; // 5 Default
+            Layer.ShadowColor = UIColor.Black.CGColor;
+            Layer.ShadowOpacity = 0.2f; // 0.8f Default
+            Layer.ShadowOffset = new CGSize(0f, 2.5f);
+
+            if (Element.OutlineColor == Xamarin.Forms.Color.Default)
+            {
+                Layer.BorderColor = UIColor.Clear.CGColor;
+            }
+            else
+            {
+                Layer.BorderColor = Element.OutlineColor.ToCGColor();
+                Layer.BorderWidth = 1;
+            }
+
+            Layer.RasterizationScale = UIScreen.MainScreen.Scale;
+            Layer.ShouldRasterize = true;
+        }
+    }
+}
